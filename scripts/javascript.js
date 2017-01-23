6942447f852c5ad78f5b6507eb8540f9afe5cfe3
Thomas Trinqué <thomas.trinque@gmail.com>
2017-01-23 15:49:07 +0100

    modif du javascript

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -4,16 +4,27 @@ $(".menuItem").hover(function() {
 		$("ul:first",this).slideUp( "400");});
 
 $(document).ready(function(){
-  $('.slider').slick({
-	  autoplay: true,
-	  infinite: true,
-	  speed: 300,
-	  slidesToShow: 1,
-	  adaptiveHeight: true,
-	  accessibility: true,
-	  arrows: true,
-	  variableWidth: false
-  });
+	$('.slider').slick({
+		autoplay: true,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 1,
+		adaptiveHeight: true,
+		accessibility: true,
+		arrows: true,
+		variableWidth: false
+	});
 });
 
-
+$(document).ready(function(){
+	$('.slider').slick({
+		autoplay: true,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 1,
+		adaptiveHeight: true,
+		accessibility: true,
+		arrows: true,
+		variableWidth: false
+	});
+});
